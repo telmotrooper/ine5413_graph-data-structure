545ee91b16a68e6a08f3aa1aada06a1362bfee68
Telmo "Trooper <telmo.trooper@gmail.com>
2017-05-22 21:52:14 -0300

    A few methods implemented

--- grafo.py
+++ grafo.py
@@ -4,6 +4,11 @@ from vertice import Vertice
 class Grafo:
     """Implementação da estrutura de dados grafo"""
 
+    '''
+    As complexidades das estruturas de dados padrão do Python estão disponíveis aqui:
+    https://www.ics.uci.edu/~pattis/ICS-33/lectures/complexitypython.txt
+    '''
+
     # Conjuntos
     v = set()
     a = set()
@@ -14,31 +19,38 @@ class Grafo:
     def __init__(self):
         print("Grafo inicializado.")
 
-    def adiciona_vertice(self):
+    def adiciona_vertice(self):  # Complexidade O(1)
         self.num_vertices += 1
         self.v.add(Vertice(self.num_vertices))
 
+    # TODO
     def remove_vertice(self):
-        print("To do!")
+        print()
 
-    def conecta(self):
-        print("To do!")
+    def conecta(self, v1, v2):  # Complexidade O(1)
+        v1.conecta(v2)
+        v2.conecta(v1)
 
+    # TODO
     def desconecta(self):
         print("To do!")
 
     def ordem(self):
-        print("To do!")
+        return len(self.v)
 
+    # TODO
     def vertices(self):
         print("To do!")
 
+    # TODO
     def um_vertice(self):
         print("To do!")
 
+    # TODO
     def adjacentes(self):
         print("To do!")
 
+    # TODO
     def grau(self):
         print("To do!")
 
@@ -46,3 +58,5 @@ class Grafo:
 if __name__ == "__main__":
     g = Grafo()
     g.adiciona_vertice()
+
+    print("O grafo é de ordem {0}.".format(g.ordem()))
--- vertice.py
+++ vertice.py
@@ -2,7 +2,11 @@ class Vertice:
     """Implementação do vértice a ser utilizada com o grafo"""
 
     numero = None
+    adjacentes = set()
 
     def __init__(self, numero):
         self.numero = numero
         print("Vértice {0} adicionado.".format(self.numero))
+
+    def conecta(self, vertice):
+        self.adjacentes.add(vertice)
