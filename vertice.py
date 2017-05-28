8296931c01211121910029682d5facf49823f2ef
Telmo "Trooper <telmo.trooper@gmail.com>
2017-05-27 22:50:39 -0300

    Added custom name field 'etiqueta' to the 'vertice' class

--- vertice.py
+++ vertice.py
@@ -2,20 +2,25 @@ class Vertice:
     """Implementação do vértice a ser utilizada com o grafo"""
 
     numero = 0  # contador estático
+    etiqueta = None  # permite dar um nome ao vértice diferente do nome padrão
 
     sucessores = None
     antecessores = None
 
-    def __init__(self):
+    def __init__(self, etiqueta=None):
         Vertice.numero += 1  # incrementa o contador de instâncias da classe
         self.numero = Vertice.numero  # atribui um número a esse vértice
+        self.etiqueta = etiqueta
 
         self.sucessores = set()
         self.antecessores = set()
 
     def __str__(self):
-        return "Vértice {0}".format(self.numero)
-        # return "Vértice {0} no endereço {1}".format(self.numero, hex(id(self)))
+        if self.etiqueta:
+            return self.etiqueta
+        else:
+            return "Vértice {0}".format(self.numero)
+            # return "Vértice {0} no endereço {1}".format(self.numero, hex(id(self)))
 
     def conecta(self, vertice):
         self.sucessores.add(vertice)
