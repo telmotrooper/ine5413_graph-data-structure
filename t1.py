fda9b58d97dc3a6c2774a65f02e5c692203e6f37
Telmo "Trooper <telmo.trooper@gmail.com>
2017-05-27 23:20:16 -0300

    Removed tests from 'grafo' and added 't1'

---
+++ t1.py
@@ -0,0 +1,105 @@
+from grafo import Grafo
+from vertice import Vertice
+
+g = Grafo()
+
+# Fase 1
+v0 = Vertice("Pré-Cálculo")
+v1 = Vertice("Circuitos e Técnicas Digitais")
+v2 = Vertice("Introdução à Computação")
+v3 = Vertice("Programação Orientada a Objetos I")
+v4 = Vertice("Fundamentos de Matemática Discreta para Computação")
+v5 = Vertice("Cálculo A")
+
+# Fase 2
+v6 = Vertice("Programação Orientada a Objetos II")
+v7 = Vertice("Probabilidade e Estatística")
+v8 = Vertice("Sistemas Digitais")
+v9 = Vertice("Ciência, Tecnologia e Sociedade")
+v10 = Vertice("Geometria Analítica")
+v11 = Vertice("Cálculo B para Computação")
+
+# Fase 3
+v12 = Vertice("Estruturas de Dados")
+v13 = Vertice("Cálculo Numérico para Computação")
+v14 = Vertice("Programação Concorrente")
+v15 = Vertice("Organização de Computadores I")
+v16 = Vertice("Álgebra Linear")
+
+
+# Fase 4
+v17 = Vertice("Sistemas Operacionais I")
+v18 = Vertice("Grafos")
+v19 = Vertice("Redes de Computadores I")
+v20 = Vertice("Teoria da Computação")
+v21 = Vertice("Paradigmas de Programação")
+v22 = Vertice("Engenharia de Software I")
+
+# Fase 5
+v23 = Vertice("Computação Distribuída")
+v24 = Vertice("Engenharia de Software II")
+v25 = Vertice("Computação Gráfica")
+v26 = Vertice("Linguagens Formais e Compiladores")
+v27 = Vertice("Redes de Computadores II")
+v28 = Vertice("Banco de Dados I")
+
+# Fase 6
+v29 = Vertice("Sistemas Operacionais II")
+v30 = Vertice("Modelagem e Simulação")
+v31 = Vertice("Construção de Compiladores")
+v32 = Vertice("Planejamento e Gestão de Projetos")
+v33 = Vertice("Inteligência Artificial")
+v34 = Vertice("Introdução ao Trabalho de Conclusão de Curso")
+
+# Fase 7
+v35 = Vertice("Informática e Sociedade")
+v36 = Vertice("Segurança em Computação")
+v37 = Vertice("Sistemas Multimídia")
+v38 = Vertice("Banco de Dados II")
+v39 = Vertice("Trabalho de Conclusão de Curso I")
+
+# Fase 8
+v40 = Vertice("Trabalho de Conclusão de Curso II")
+
+# Adicionando os vértices no grafo
+g.adiciona_vertice(v0)
+g.adiciona_vertice(v1)
+g.adiciona_vertice(v2)
+g.adiciona_vertice(v3)
+g.adiciona_vertice(v4)
+g.adiciona_vertice(v5)
+g.adiciona_vertice(v6)
+g.adiciona_vertice(v7)
+g.adiciona_vertice(v8)
+g.adiciona_vertice(v9)
+g.adiciona_vertice(v10)
+g.adiciona_vertice(v11)
+g.adiciona_vertice(v12)
+g.adiciona_vertice(v13)
+g.adiciona_vertice(v14)
+g.adiciona_vertice(v15)
+g.adiciona_vertice(v16)
+g.adiciona_vertice(v17)
+g.adiciona_vertice(v18)
+g.adiciona_vertice(v19)
+g.adiciona_vertice(v20)
+g.adiciona_vertice(v21)
+g.adiciona_vertice(v22)
+g.adiciona_vertice(v23)
+g.adiciona_vertice(v24)
+g.adiciona_vertice(v25)
+g.adiciona_vertice(v26)
+g.adiciona_vertice(v27)
+g.adiciona_vertice(v28)
+g.adiciona_vertice(v29)
+g.adiciona_vertice(v30)
+g.adiciona_vertice(v31)
+g.adiciona_vertice(v32)
+g.adiciona_vertice(v33)
+g.adiciona_vertice(v34)
+g.adiciona_vertice(v35)
+g.adiciona_vertice(v36)
+g.adiciona_vertice(v37)
+g.adiciona_vertice(v38)
+g.adiciona_vertice(v39)
+g.adiciona_vertice(v40)
